6ef5c0124864688f6e4bed4d43b97d536bc013e2
Samuell02 <samuecaalves@Hotmail.com>
2023-05-16 14:11:48 -0300

    s

--- untitled/src/CartaoDeCredito.java
+++ untitled/src/CartaoDeCredito.java
@@ -1,5 +1,14 @@
 public class CartaoDeCredito extends  Pagamento{
+    int senha;
+    double saldo;
+    double preço;
     public void realizarPagamento(){
+        System.out.println("Insira a senha:");
+        System.out.println(preço);
+
+        System.out.println(saldo-preço);
+
+
 
     }
     public void EmitirRecibo(){
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Main {
+    int senha;
+    double saldo;
+    double preço;
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
     }
 }
