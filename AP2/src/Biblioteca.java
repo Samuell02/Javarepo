9a52765113f303de67828bce708766b1ccc7c135
Samuell02 <samuecaalves@Hotmail.com>
2023-05-18 16:03:33 -0300

    sus

---
+++ AP2/src/Biblioteca.java
@@ -0,0 +1,9 @@
+public class Biblioteca {
+    Livro livro1 = new Livro();
+    Livro livro2 = new Livro();
+    Livro livro3 = new Livro();
+    Livro livro4 = new Livro();
+    Livro livro5 = new Livro();
+
+
+}
