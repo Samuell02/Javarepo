f9f5a746d8aa5b20485d435f27979caded1ca411
Samuell02 <samuecaalves@Hotmail.com>
2023-05-20 19:12:04 -0300

    sus

--- AP2/src/Biblioteca.java
+++ AP2/src/Biblioteca.java
@@ -1,23 +1,25 @@
-public class Biblioteca extends Livro{
-    Livro livro1 = new Livro();
-    livro1.setTítulo("s");
- String comparar;
-    Livro livro2 = new Livro();
-    Livro livro3 = new Livro();
-    Livro livro4 = new Livro();
-    Livro livro5 = new Livro();
+public class Biblioteca {
+    Livro livro1;
+    Livro livro2;
+    Livro livro3;
+    Livro livro4;
+    Livro livro5;
 
-    public void AdicionarLivro(String novoTítulo, String  novovolume,String novoGenero,){
-        comparar= (livro1.getTítulo());
-        if (comparar!= null && comparar!=""){
-
-        }
-        else{
-       livro1.setTítulo(novoTítulo,novovolume);
-       livro1.setGenero(novoGenero);
-       livro1.
+    public void adicionarLivro1(Livro livro) {
+        if (livro != null && livro.getTítulo() != null && !livro.getTítulo().isEmpty()) {
+            this.livro1 = livro;
+        } else {
+            System.out.println("Não é possível adicionar um livro sem título ao slot 1.");
         }
     }
 
+    public void adicionarLivro2(Livro livro) {
+        if (livro != null && livro.getTítulo() != null && !livro.getTítulo().isEmpty()) {
+            this.livro2 = livro;
+        } else {
+            System.out.println("Não é possível adicionar um livro sem título ao slot 2.");
+        }
+    }
 
+    // Métodos semelhantes para adicionarLivro3, adicionarLivro4 e adicionarLivro5...
 }
