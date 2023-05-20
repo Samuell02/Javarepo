13c0ca67696b8281cced6170ad553e87b4476e2c
Samuell02 <samuecaalves@Hotmail.com>
2023-05-20 16:48:13 -0300

    sus

--- AP2/src/Biblioteca.java
+++ AP2/src/Biblioteca.java
@@ -7,13 +7,15 @@ public class Biblioteca extends Livro{
     Livro livro4 = new Livro();
     Livro livro5 = new Livro();
 
-    public void AdicionarLivro(){
+    public void AdicionarLivro(String novoTítulo, String  novovolume,String novoGenero,){
         comparar= (livro1.getTítulo());
         if (comparar!= null && comparar!=""){
 
         }
         else{
-       livro1.setTítulo("despacito","1");
+       livro1.setTítulo(novoTítulo,novovolume);
+       livro1.setGenero(novoGenero);
+       livro1.
         }
     }
 
