c8dab017d8e57a61fd7fb8ecb80a7e32022a851d
Samuell02 <samuecaalves@Hotmail.com>
2023-05-21 18:46:55 -0300

    sus

--- AP2/src/Main.java
+++ AP2/src/Main.java
@@ -22,7 +22,7 @@ public class Main   {
         System.out.println(livro3.detalhesDoProduto());
         System.out.println(livro3.getGenero());
         System.out.println(livro3.getEditora());
-        biblioteca1.removerLivro("Dito e feito ");
+        biblioteca1.removerLivro("A casa amarela");
         System.out.println(livro1.getTítulo());
         System.out.println(livro1.detalhesDoProduto());
 
