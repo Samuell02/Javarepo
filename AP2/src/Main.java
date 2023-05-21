e000717f18f1a286e7838ea477d3003a262ff6f4
Samuell02 <samuecaalves@Hotmail.com>
2023-05-21 19:25:53 -0300

    sus

--- AP2/src/Main.java
+++ AP2/src/Main.java
@@ -4,7 +4,7 @@ public class Main   {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-           Livro livro1 =  new Livro("Dito e feito ", "Jennifer Armstrong / Kimberly Bulcken Root","2006","Sim","Unico");
+           Livro livro1 =  new Livro("Dito e feito", "Jennifer Armstrong / Kimberly Bulcken Root","2006","Sim","Unico");
            livro1.setGenero("infantil");
            livro1.setEditora("Brinque Book");
         Livro livro2 =  new Livro("O ladrão de raios","Rick Riordan","2005","Não","1 de 5");
@@ -13,8 +13,11 @@ public class Main   {
         Livro livro3 =  new Livro("","","","","");
 
         Livro livro4 =  new Livro("O Mágico de Oz","L. Frank Braum","2017(Publicação original em  1900)","Não","Unico");
-
-        Livro livro5 =  new Livro("","","","","");
+        livro4.setGenero("Alta Fantasia");
+        livro4.setEditora("Companhia de Bolso");
+        Livro livro5 =  new Livro("Capitaos da areia","Jorge Amadu ","2009(Publicação original em 1912)","Sim","Unico");
+        livro5.setGenero("Drama");
+        livro5.setEditora("Companhia de Bolso");
 
         Biblioteca biblioteca1 = new Biblioteca(livro1,livro2,livro3,livro4,livro5);
 
@@ -23,8 +26,15 @@ public class Main   {
         System.out.println(livro3.getGenero());
         System.out.println(livro3.getEditora());
         biblioteca1.removerLivro("A casa amarela");
-        System.out.println(livro1.getTítulo());
-        System.out.println(livro3.detalhesDoProduto());
+        biblioteca1.AtualizarLivro5(livro5 ,"Capitães da areia","Jorge Amado ","Drama","2009(Publicação original em 1937)","Jorge Amado","Não","Companhia de Bolso");
+        System.out.println(biblioteca1.livro4.detalhesDoProduto());
+        System.out.println(biblioteca1.livro4.getGenero());
+        System.out.println(biblioteca1.livro4.getEditora());
+        System.out.println(biblioteca1.livro3.detalhesDoProduto());
+        System.out.println(biblioteca1.livro5.detalhesDoProduto());
+        System.out.println(biblioteca1.livro5.getGenero());
+        System.out.println(biblioteca1.livro5.getEditora());
+
 
     }
 }
